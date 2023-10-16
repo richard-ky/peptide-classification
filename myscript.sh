2ce038b4062837aeb187949378d15125ece85235
Richard Ky <richardky30@gmail.com>
2023-10-16 00:35:46 -0700

    update

--- myscript.sh
+++ myscript.sh
@@ -4,12 +4,11 @@
 #SBATCH --partition=gpu
 #SBATCH --output=output.txt
 #
-#SBATCH --cpus-per-task=56
+#SBATCH --cpus-per-task=4
 #SBATCH --time=48:00:00
-#SBATCH --mem-per-cpu=4000
+#SBATCH --mem-per-cpu=1000
 #SBATCH --mail-user=richard.ky@sjsu.edu
 #SBATCH --mail-type=BEGIN,END
 
-module load python3
-
-python prott5_predictions.py 'Small Peptide Hits'
+module load anaconda
+conda run -n fastaenv python prott5_predictions.py 'Small Peptide Hits'
