9ab722dd8e87b245486cd109b5c320967d302311
Richard Ky <richardky30@gmail.com>
2023-10-15 23:13:57 -0700

    test

--- test_bash_script.sh
+++ test_bash_script.sh
@@ -4,9 +4,9 @@
 #SBATCH --partition=gpu
 #SBATCH --output=output.txt
 #
-#SBATCH --cpus-per-task=56
+#SBATCH --cpus-per-task=4
 #SBATCH --time=48:00:00
-#SBATCH --mem-per-cpu=4000
+#SBATCH --mem-per-cpu=1000
 #SBATCH --mail-user=richard.ky@sjsu.edu
 #SBATCH --mail-type=BEGIN,END
 
